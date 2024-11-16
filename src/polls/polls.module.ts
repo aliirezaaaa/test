bd49c9fb13f3258f8d6f0c2fed8d80260caf7ecc
Alireza Khorramshekooh <alireza.khorramshekooh@gmail.com>
2024-11-16 05:24:57 +0330

    redis module add

--- src/polls/polls.module.ts
+++ src/polls/polls.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
+import { ConfigModule } from '@nestjs/config';
+import { redisModule } from 'src/modules.config';
 
 @Module({
+  imports: [ConfigModule, redisModule],
   controllers: [PollsController],
   providers: [PollsService],
 })
-export class PollsModule {}
+export class PollsModule { }
--- src/polls/polls.service.ts
+++ src/polls/polls.service.ts
@@ -1,18 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { createPollID, createUserID } from 'src/ids';
 import { CreatePollFields, JoinPollFields, RejoinPollFields } from './types';
 @Injectable()
 export class PollsService {
-        async createPoll(fields: CreatePollFields) {
-            const pollID = createPollID();
-            const userID = createUserID();
 
-            return {
-                ...fields,
-                userID,
-                pollID,
-            };
-        }
+    async createPoll(fields: CreatePollFields) {
+        Logger.log('from pollservice and create poll')
+        const pollID = createPollID();
+        const userID = createUserID();
+
+        return {
+            ...fields,
+            userID,
+            pollID,
+        };
+    }
 
     async joinPoll(fields: JoinPollFields) {
         const userID = createUserID();
