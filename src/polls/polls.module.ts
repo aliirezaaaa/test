2a2cb9a2be82f5a085c2b20ec19f2b27df6b6bce
Alireza Khorramshekooh <alireza.khorramshekooh@gmail.com>
2024-11-16 05:33:08 +0330

    poll repository add

--- src/polls/polls.module.ts
+++ src/polls/polls.module.ts
@@ -3,10 +3,11 @@ import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
 import { ConfigModule } from '@nestjs/config';
 import { redisModule } from 'src/modules.config';
+import { PollsRepository } from 'src/polls/polls.repository';
 
 @Module({
   imports: [ConfigModule, redisModule],
   controllers: [PollsController],
-  providers: [PollsService],
+  providers: [PollsService, PollsRepository],
 })
 export class PollsModule { }
---
+++ src/polls/polls.repository.ts
@@ -0,0 +1,111 @@
+import { Inject, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Redis } from 'ioredis';
+import { IORedisKey } from 'src/redis.module';
+import { AddParticipantData, CreatePollData } from './types';
+import { Poll } from './poll.interface';
+
+@Injectable()
+export class PollsRepository {
+    private readonly ttl: string;
+    private readonly logger = new Logger(PollsRepository.name);
+
+    constructor(
+        configService: ConfigService,
+        @Inject(IORedisKey) private readonly redisClient: Redis,
+    ) {
+        this.ttl = configService.get('POLL_DURATION');
+    }
+
+    async createPoll({
+        votesPerVoter,
+        topic,
+        pollID,
+        userID,
+    }: CreatePollData): Promise<Poll> {
+        const initialPoll = {
+            id: pollID,
+            topic,
+            votesPerVoter,
+            participants: {},
+            adminID: userID,
+        };
+
+        this.logger.log(
+            `Creating new poll: ${JSON.stringify(initialPoll, null, 2)} with TTL ${this.ttl}`,
+        );
+
+        const key = `polls:${pollID}`;
+
+        try {
+            await this.redisClient
+                .multi()
+                .set(key, JSON.stringify(initialPoll))
+                .expire(key, parseInt(this.ttl))
+                .exec();
+            return initialPoll;
+        } catch (e) {
+            this.logger.error(
+                `Failed to add poll ${JSON.stringify(initialPoll)}\n${e}`,
+            );
+            throw new InternalServerErrorException();
+        }
+    }
+
+    async getPoll(pollID: string): Promise<Poll> {
+        this.logger.log(`Attempting to get poll with: ${pollID}`);
+
+        const key = `polls:${pollID}`;
+
+        try {
+            const currentPoll = await this.redisClient.sendCommand(
+                new Redis.Command('JSON.GET', [key, '.']),
+            );
+
+            this.logger.verbose(currentPoll);
+
+            return JSON.parse(currentPoll as string);
+        } catch (e) {
+            this.logger.error(`Failed to get pollID ${pollID}`);
+            throw e;
+        }
+    }
+
+    async addParticipant({
+        pollID,
+        userID,
+        name,
+    }: AddParticipantData): Promise<Poll> {
+        this.logger.log(
+            `Attempting to add a participant with userID/name: ${userID}/${name} to pollID: ${pollID}`,
+        );
+
+        const key = `polls:${pollID}`;
+        const participantPath = `.participants.${userID}`;
+
+        try {
+            await this.redisClient.sendCommand(
+                new Redis.Command('JSON.SET', [key, participantPath, JSON.stringify(name)]),
+            );
+
+            const pollJSON = await this.redisClient.sendCommand(
+                new Redis.Command('JSON.GET', [key, '.']),
+            );
+
+            const poll = JSON.parse(pollJSON as string) as Poll;
+
+            this.logger.debug(
+                `Current Participants for pollID: ${pollID}:`,
+                poll.participants,
+            );
+
+            return poll;
+        } catch (e) {
+            this.logger.error(
+                `Failed to add a participant with userID/name: ${userID}/${name} to pollID: ${pollID}`,
+            );
+            throw e;
+        }
+    }
+}
