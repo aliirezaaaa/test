46c6cfc3fb206dbe09e5adc31b9a5c9d21ead943
Alireza Khorramshekooh <alireza.khorramshekooh@gmail.com>
2024-11-17 13:09:43 +0330

    structure of project changed. remove shared and client

--- dist/polls/dtos.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreatePollDto {
-    topic: string;
-    votesPerVoter: number;
-    name: string;
-}
-export declare class JoinPollDto {
-    pollID: string;
-    name: string;
-}
