d011bf00ba53eb8cbb12c72f9ad2b715b35c9977
Alireza Khorramshekooh <alireza.khorramshekooh@gmail.com>
2024-11-17 13:02:36 +0330

    jwt add

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
 import { ConfigModule } from '@nestjs/config';
-import { redisModule } from 'src/modules.config';
+import { jwtModule, redisModule } from 'src/modules.config';
 import { PollsRepository } from 'src/polls/polls.repository';
 
 @Module({
-  imports: [ConfigModule, redisModule],
+  imports: [ConfigModule, redisModule, jwtModule],
   controllers: [PollsController],
   providers: [PollsService, PollsRepository],
 })
